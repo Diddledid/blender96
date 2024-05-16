1debc8bbe4cb48ee8db105302c7cac3612d58889
Diddledid <154475642+Diddledid@users.noreply.github.com>
2024-05-16 12:44:06 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-//Placeholder
+const config = {
+  challenge: true, // If challenge is true, then it enables password protection.
+  users: {
+    lalala: "schoolsucks",
+  },
+}
+export default config
